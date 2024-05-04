cd077f1505fa68265cd133bb12d77fd87796d53a
YugaNamba <y.nannba@patternstorage.com>
2024-05-04 16:41:41 +0900

    feat: 🎸 refactor

--- src/customers.rs
+++ src/customers.rs
@@ -1,14 +1,15 @@
 use crate::AppError;
+use anyhow::Result;
 use axum::{extract::Path, Json};
-use gcp_bigquery_client::model::query_request::QueryRequest;
-use serde::Serialize;
-use serde_json::Value;
+use gcp_bigquery_client::model::{query_request::QueryRequest, query_response::ResultSet};
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 use crate::init_bq_client;
-#[derive(Debug, Serialize, Clone, ToSchema)]
+
+#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
 pub struct Customer {
-    id: String,
+    customer_id: String,
     first_name: String,
     last_name: String,
     email: String,
@@ -25,7 +26,7 @@ pub async fn list() -> Result<Json<Vec<Customer>>, AppError> {
         "SELECT *  FROM `{}.{}.{}` ORDER BY customer_id ASC LIMIT 1000",
         gcp_project_id, "test_tokyo", "customers"
     );
-    let rs = client
+    let mut rs = client
         .job()
         .query(gcp_project_id, QueryRequest::new(&query))
         .await
@@ -34,8 +35,11 @@ pub async fn list() -> Result<Json<Vec<Customer>>, AppError> {
             AppError(anyhow::anyhow!(e))
         })?;
 
-    let customers = parse_customers(rs.query_response().rows.as_ref());
-
+    let mut customers: Vec<Customer> = Vec::new();
+    while rs.next_row() {
+        let customer = parse_to_customer(&rs)?;
+        customers.push(customer);
+    }
     Ok(Json(customers))
 }
 
@@ -49,7 +53,7 @@ pub async fn get(Path(id): Path<String>) -> Result<Json<Customer>, AppError> {
         "SELECT *  FROM `{}.{}.{}` WHERE customer_id = {} LIMIT 1",
         gcp_project_id, "test_tokyo", "customers", id
     );
-    let rs = client
+    let mut rs = client
         .job()
         .query(gcp_project_id, QueryRequest::new(&query))
         .await
@@ -58,36 +62,42 @@ pub async fn get(Path(id): Path<String>) -> Result<Json<Customer>, AppError> {
             AppError(anyhow::anyhow!(e))
         })?;
 
-    let customers = parse_customers(rs.query_response().rows.as_ref());
+    let mut customers: Vec<Customer> = Vec::new();
+    while rs.next_row() {
+        let customer = parse_to_customer(&rs)?;
+        customers.push(customer);
+    }
 
     Ok(Json(customers[0].clone()))
 }
 
-fn parse_customers(
-    rows: Option<&Vec<gcp_bigquery_client::model::table_row::TableRow>>,
-) -> Vec<Customer> {
-    rows.unwrap_or(&Vec::new())
-        .iter()
-        .filter_map(|row| row.columns.as_ref())
-        .map(|columns| {
-            let id = get_cleaned_value(&columns[0].value);
-            let first_name = get_cleaned_value(&columns[1].value);
-            let last_name = get_cleaned_value(&columns[2].value);
-            let email = get_cleaned_value(&columns[3].value);
-            let created_at = get_cleaned_value(&columns[4].value);
-
-            Customer {
-                id,
-                first_name,
-                last_name,
-                email,
-                created_at,
-            }
-        })
-        .collect()
-}
-
-fn get_cleaned_value(opt: &Option<Value>) -> String {
-    opt.as_ref()
-        .map_or_else(String::new, |v| v.to_string().trim_matches('"').to_string())
+fn parse_to_customer(row: &ResultSet) -> Result<Customer, AppError> {
+    let email = row
+        .get_string_by_name("email")
+        .unwrap_or_default()
+        .unwrap_or_default();
+    let first_name = row
+        .get_string_by_name("first_name")
+        .unwrap_or_default()
+        .unwrap_or_default();
+    let last_name = row
+        .get_string_by_name("last_name")
+        .unwrap_or_default()
+        .unwrap_or_default();
+    let created_at = row
+        .get_string_by_name("created_at")
+        .unwrap_or_default()
+        .unwrap_or_default();
+    let customer_id = row
+        .get_string_by_name("customer_id")
+        .unwrap_or_default()
+        .unwrap_or_default();
+    let customer = Customer {
+        email,
+        first_name,
+        last_name,
+        created_at,
+        customer_id,
+    };
+    Ok(customer)
 }
